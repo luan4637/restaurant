5f786ef14567b0238af19f2fadeb00dea6bc4872
Luan Pham <luan4637@gmail.com>
2025-05-04 12:14:02 +0700

    build

--- src/app/routers/restaurantPage.ts
+++ src/app/routers/restaurantPage.ts
@@ -1,11 +1,14 @@
-import { Router, Request, Response } from 'express';
+import express, { Router, Request, Response } from 'express';
 import { RestaurantController } from '../controllers/RestaurantController';
+
+const path = require('path');
+const rootPath = path.resolve('./');
 
 export const restaurantPage = Router();
 
+restaurantPage.use(express.static(rootPath + '/dist/public/'));
 restaurantPage.get('/', (req: Request, res: Response, next) => {
-    const restaurantController = new RestaurantController(req, res);
-    restaurantController.index();
+    res.sendFile(rootPath + '/dist/public/' + 'index.html');
 });
 
 restaurantPage.get('/restaurant', (req: Request, res: Response, next) => {
--- src/infrastructure/dataSource/AppDataSource.ts
+++ src/infrastructure/dataSource/AppDataSource.ts
@@ -7,14 +7,17 @@ const process = require('process');
 const rootPath = path.resolve('./');
 const env = process.env.NODE_ENV || 'development';
 const config = require(rootPath + '/config/config.json')[env];
+const configtest = require(rootPath + '/config/config.json');
+
+console.log(configtest);
 
 export const AppDataSource = new DataSource({
-    type: config.dialect,
+    type: config.dialect || 'mysql',
     host: config.host || 'localhost',
     port: config.port || 3306,
-    username: config.username,
-    password: config.password,
-    database: config.database,
+    username: config.username || 'restaurant_user',
+    password: config.password || 'restaurant_password',
+    database: config.database || 'restaurant_db',
     entities: [
         RestaurantModel
     ],
